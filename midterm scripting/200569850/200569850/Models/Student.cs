ddc646247c0e210aa015f32b0bf43628992ef295
sarun7777 <sarung90t@gmail.com>
2024-06-19 11:16:04 -0400

    Add files via upload

---
+++ midterm scripting/200569850/200569850/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Student
+{
+    [Key]
+    public int StudentId { get; set; }
+
+    [Required]
+    public string FirstName { get; set; }
+
+    [Required]
+    public string LastName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string EmailAddress { get; set; }
+}
---
+++ midterm scripting/200569850/200569850/Properties/data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YourNamespace.Models;
+
+namespace YourNamespace.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+        // Other DbSet properties for your other models, if any
+    }
+}
